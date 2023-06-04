ae215b651a0d037ea8c0215bce080e14892a1f04
jinhan38 <jinhan38@naver.com>
2023-06-04 14:11:38 +0900

    변수 추가

---
+++ lib/ch1/variable.dart
@@ -0,0 +1,16 @@
+class Variable {
+
+  Variable() {
+    String name = "김진한";
+    print(name);
+    double weight = 75.6;
+    bool check = false;
+    print(check);
+
+    int age = 33;
+    print(age);
+    age = 50;
+    print(age);
+  }
+
+}
